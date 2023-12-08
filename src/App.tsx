65a870fe20a69a08fc451046d55516faf0c11988
Javokhir <jtursunboev@bk.ru>
2023-12-08 22:27:12 +0900

    Add react-router-dom and update main.tsx and package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import "./globals.css";
+import SignInForm from "./_auth/forms/SignInForm";
+import { Home } from "./_root/pages";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/forms/AuthLayout";
+import RootLayout from "./_root/pages/RootLayout";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public root */}
+
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+
+        {/* privete routes */}
+        <Route element={<RootLayout />}>
+          <Route path="/" index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,11 @@
 import ReactDOM from "react-dom";
 import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+ReactDOM.render(<RouterProvider router={router} />, document.getElementById("root"));
