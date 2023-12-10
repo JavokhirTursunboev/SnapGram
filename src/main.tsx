2c6f954b7bec09a72a52422a490a76b216a59abd
Javokhir <jtursunboev@bk.ru>
2023-12-10 17:08:55 +0900

    Add button component to SignUpForm and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import ReactDOM from "react-dom";
-import App from "./App";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
-ReactDOM.render(<RouterProvider router={router} />, document.getElementById("root"));
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
